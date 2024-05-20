78d8d6b2673400fc071c5825e6656f1de0eea38e
jc-alvarez-fdez <jc.alvarez.fdez@gmail.com>
2024-05-20 11:27:10 +0200

    protección de las rutas con  guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { ObtenMedicacionComponent } from './pages/obten_medicacion/obten_medicacion.component';
 import { MisMedicamentosComponent } from './pages/mis-medicamentos/mis-medicamentos.component';
 import { EstadisticasComponent } from './pages/estadisticas/estadisticas.component';
+import { AuthGuard } from './core/utils/guards/auth.guard';
 
 export const routes: Routes = [
 
@@ -12,9 +13,9 @@ export const routes: Routes = [
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'obten_medicacion', component: ObtenMedicacionComponent },
-  { path: 'mis_medicamentos', component: MisMedicamentosComponent },
-  { path: 'estadisticas', component: EstadisticasComponent },
-
+  { path: 'obten_medicacion', component: ObtenMedicacionComponent, canActivate: [AuthGuard] },
+  { path: 'mis_medicamentos', component: MisMedicamentosComponent, canActivate: [AuthGuard] },
+  { path: 'estadisticas', component: EstadisticasComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 
 ];
--- src/app/core/utils/guards/auth.guard.ts
+++ src/app/core/utils/guards/auth.guard.ts
@@ -1,3 +1,36 @@
+// Basado en sprint 7
+
+import { Injectable } from '@angular/core';
+import { Router, RouterStateSnapshot, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Injectable({ providedIn: 'root' })
+
+
+export class AuthGuard {
+  constructor(
+    private router: Router,
+    private _authService: AuthService
+  ) {}
+
+  canActivate(state: RouterStateSnapshot, activatedRoute: ActivatedRoute): boolean {
+    const paciente = this._authService.pacienteValue;
+        if (paciente) {
+            // authorised so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/auth/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
+
+
+
+
+/* Anterior
+
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -24,3 +57,5 @@ export class AuthGuard {
   }
 
 }
+
+*/
